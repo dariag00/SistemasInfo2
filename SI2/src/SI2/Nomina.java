7e022422e4ef5a2f51d740b2edf76c91a28a8eb9
markhop1 <markhopp11@gmail.com>
2018-05-22 20:48:51 +0200

    Subiendo Nómina

---
+++ SI2/src/SI2/Nomina.java
@@ -0,0 +1,51 @@
+package SI2;
+
+public class Nomina {
+    
+    int mes; 
+    int anio; 
+    int numeroTrienios;
+    double importeTrienios; 
+    double importeSalarioMes;
+    double importeComplementoMes;
+    double valorProrrateo;
+    double brutoAnual;
+    double IRPF;
+    double importeIPRF;
+    double baseEmpresario;
+    double seguridadSocialEmpresario;
+    double importeSeguridadSocialEmpresario;
+    double desempleoEmpresario;
+    double importeDesempleoEmpresario;
+    double formacionEmpresario;
+    double importeFormacionEmpresario;
+    double accidentesTrabajoEmpresario;
+    double importeAccidentesTrabajo;
+    double FOGASAEmpresario; 
+    double importeFOGASAEmpresario;
+    double seguridadSocialTrabajador;
+    double importeSeguridadSocialTrabajador;
+    double desempleoTrabajador;
+    double importeDesempleoTrabajador;
+    double formacionTrabajador;
+    double importeFormacionTrabajador;
+    double brutoNomina; 
+    double liquidoNomina;
+    //FALTAN 2
+    //FALTAN 2
+
+    
+    public Empresa(){
+        
+    }
+
+    
+
+    @Override
+    public String toString() {
+        return "Empresa{" + "direccion=" + direccion + ", CIF=" + CIF + ", nombre=" + nombre + '}';
+    }
+    
+    
+    
+}
